227ef4bc01c80c098da1fced02dfff2598515247
Ederson S <edersonrodrigo31@gmail.com>
2020-12-15 11:39:25 -0300

    Create  products migration

---
+++ backend/src/database/migrations/01_products.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('products', table => {
+    table.increments('id').primary()
+    table.string('title').notNullable()
+    table.string('description').notNullable()
+    table.string('category').notNullable()
+    table.string('price').notNullable()
+    table.string('image').notNullable()
+
+    table.integer('company_id').notNullable()
+      .references('id').inTable('companies')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+  })
+} 
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('products')
+}  
