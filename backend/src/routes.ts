a61ad74742629db6f3c0fc87c8d166656de8c3aa
Ederson S <edersonrodrigo31@gmail.com>
2020-12-15 17:02:42 -0300

    Create adm route and function index in adm controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express'
-import companyController from './controllers/FisrtRegistCompanyController'
+import admController from './controllers/admController'
+import companyController from './controllers/CompanyController'
+
 
 const routes = express.Router()
 const companycontroller = new companyController()
+const admcontroller = new admController()
 
 routes.post('/newcompany', companycontroller.create)
+routes.get('/adm', admcontroller.index)
 
 export default routes
